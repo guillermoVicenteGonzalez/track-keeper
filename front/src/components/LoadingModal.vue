8a76dfc7fa8eafb0c55fd6f442418f293df0f05c
guillermoVicenteGonzalez <guillermovg@usal.es>
2023-09-05 21:02:50 +0200

    loading modal

---
+++ front/src/components/LoadingModal.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-dialog
+    persistent :overlay="false"
+    max-width="300px"
+    transition="dialog-transition">
+            <v-card 
+            min-height="100px"
+            height="100" class="justify-center align-center text-center">
+                <v-progress-circular
+                    indeterminate
+                    color="primary"
+                    class="justify-center"
+                ></v-progress-circular>
+            </v-card>
+    </v-dialog>
+</template>
+
+<script setup>
+import {ref } from "vue"
+
+
+</script>
--- front/src/views/LandingPage.vue
+++ front/src/views/LandingPage.vue
@@ -1,22 +1,4 @@
 <template>
-<!--
-  <v-container class="fill-height">
-    <v-responsive class="align-center text-center fill-height">
-      <v-row class="d-block align-center justify-center">
-        <v-col>
-          <h1>Hola</h1>
-        </v-col>
-        <v-col class="align-center justify-">
-          <v-btn
-          @click="router.push('/signup')">Sign up</v-btn>
-          <v-btn>Sign in</v-btn>
-        </v-col>
-      </v-row>
-    </v-responsive>
-  </v-container>
--->
-
-
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
       <router-view></router-view>
@@ -25,9 +7,6 @@
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
-
-var router = useRouter()
 
   
 </script>
