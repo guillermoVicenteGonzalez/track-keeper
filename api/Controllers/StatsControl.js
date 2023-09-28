0fe3d42b6b1cda6e6c55b15df5045c4cb6b3b538
guillermoVicenteGonzalez <guillermovg@usal.es>
2023-09-28 12:52:17 +0200

    problem solved?

--- api/Controllers/StatsControl.js
+++ api/Controllers/StatsControl.js
@@ -107,7 +107,7 @@ exports.getFavouriteGenres = async function(req,res){
      */
     for(let i in genres){
         let count = 1;
-        let j = Number(i + 1); //if not it is treated as a string??????
+        let j = Number(i) + 1; //if not it is treated as a string??????
         //let j = 1;
         let name = genres[i]
         
