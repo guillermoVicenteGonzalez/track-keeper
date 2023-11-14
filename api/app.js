474aad58ff1b50fee03e601874b400ae7d9995f8
guillermoVicenteGonzalez <guillermovg@usal.es>
2023-11-14 20:48:00 +0100

    https

--- api/app.js
+++ api/app.js
@@ -1,4 +1,6 @@
 const   express         = require("express"),
+        https           = require("https"),
+        http            = require("http"),
         app             = express(),
         bodyParser      = require("body-parser"),
         methodOverride  = require("method-override"),
@@ -11,6 +13,11 @@ const   express         = require("express"),
                           require("dotenv").config({path:"../.env"})
 
 const usersCtrl = require("./Controllers/UserCtrl")
+
+//ssl
+let privateKey = fs.readFileSync(process.env.SSL_KEY)
+let certificate = fs.readFileSync(process.env.SSL_CERT)
+var credentials = {key:privateKey,cert:certificate}
 
 const expressSwagger = require("express-swagger-generator")(app)
 let swOptions = {
@@ -51,5 +58,12 @@ app.use("/api",router)
 associations.synchronize();
 
 let port = process.env.API_PORT
+let httpsPort = process.env.API_HTTPS_PORT
+
+let httpServer = http.createServer(app).listen(port)
+let httpsServer = https.createServer(credentials,app).listen(httpsPort)
+
+/*
 console.log(port)
 app.listen(process.env.PORT || port, ()=> console.log("Server is running in port: " + process.env.API_PORT))
+*/
