667d6b16e2ce097487a108cb99538f0d315a6df6
guillermoVicenteGonzalez <guillermovg@usal.es>
2024-11-05 09:55:46 +0100

    eliminated native https

--- api/app.js
+++ api/app.js
@@ -1,68 +1,58 @@
-const   express         = require("express"),
-        https           = require("https"),
-        http            = require("http"),
-        app             = express(),
-        bodyParser      = require("body-parser"),
-        methodOverride  = require("method-override"),
-        cors            = require("cors"),
-        database        = require("./Database/database"),
-        router          = require("./Routes/index"),
-        path            = require("path"),
-        associations    = require("./Models/Associations")
-        fs              = require("fs");
-        winston         = require("./logger/logger")
-                          require("dotenv").config({path:"../.env"})
+const express = require("express"),
+  http = require("http"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  methodOverride = require("method-override"),
+  cors = require("cors"),
+  database = require("./Database/database"),
+  router = require("./Routes/index"),
+  path = require("path"),
+  associations = require("./Models/Associations");
+fs = require("fs");
+winston = require("./logger/logger");
+require("dotenv").config({ path: "../.env" });
 
-const usersCtrl = require("./Controllers/UserCtrl")
+const usersCtrl = require("./Controllers/UserCtrl");
 
 //ssl
-let privateKey = fs.readFileSync(process.env.SSL_KEY)
-let certificate = fs.readFileSync(process.env.SSL_CERT)
-var credentials = {key:privateKey,cert:certificate}
 
-const expressSwagger = require("express-swagger-generator")(app)
+const expressSwagger = require("express-swagger-generator")(app);
 let swOptions = {
-    swaggerDefinition: {
-        info: {
-            description: 'Api ',
-            title: 'Swagger',
-            version: '1.0.0',
-        },
-        host: process.env.API_HOST  + ":" + process.env.API_PORT,
-        basePath: '/',
-        produces: [
-            "application/json",
-            "application/xml"
-        ],
-        schemes: ['http', 'https'],
-        securityDefinitions: {
-            JWT: {
-                type: 'apiKey',
-                in: 'header',
-                name: 'Authorization',
-                description: "",
-            }
-        }
+  swaggerDefinition: {
+    info: {
+      description: "Api ",
+      title: "Swagger",
+      version: "1.0.0",
     },
-    basedir: "./",
-    //files: ['./controllers/usuariosCtrl.js'] //Path to the API handle folder
-    files: ['./controllers/*', './models/*']
+    host: process.env.API_HOST + ":" + process.env.API_PORT,
+    basePath: "/",
+    produces: ["application/json", "application/xml"],
+    schemes: ["http", "https"],
+    securityDefinitions: {
+      JWT: {
+        type: "apiKey",
+        in: "header",
+        name: "Authorization",
+        description: "",
+      },
+    },
+  },
+  basedir: "./",
+  //files: ['./controllers/usuariosCtrl.js'] //Path to the API handle folder
+  files: ["./controllers/*", "./models/*"],
 };
 expressSwagger(swOptions);
 
-
-app.use(bodyParser.json())
-app.use(methodOverride())
-app.use(cors())
-app.use("/api",router)
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(cors());
+app.use("/api", router);
 
 associations.synchronize();
 
-let port = process.env.API_PORT
-let httpsPort = process.env.API_HTTPS_PORT
+let port = process.env.API_PORT;
 
-let httpServer = http.createServer(app).listen(port)
-let httpsServer = https.createServer(credentials,app).listen(httpsPort)
+let httpServer = http.createServer(app).listen(port);
 
 /*
 console.log(port)
